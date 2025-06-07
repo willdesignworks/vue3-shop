40808286772cd886fffe902fb83873ee4748b4ab
will <willwind0414@gmail.com>
2025-06-07 11:54:30 +0800

    git init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/vue3-shop/', // 這裡填 GitHub 專案名稱
   plugins: [vue()],
 })
