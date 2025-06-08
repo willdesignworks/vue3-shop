b8ee36fc3c5441533b4794e4dd89981e797f561b
will <willwind0414@gmail.com>
2025-06-08 14:34:27 +0800

    git init

--- src/components/OffsetWrapper.vue
+++ src/components/OffsetWrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="offset__wrapper">
-    <div :class="['shopping__cart', { 'shopping__cart__on': isCartOpen }]">
+    <div :class="['shopping__cart', { 'shopping__cart__on': cartStore.isCartOpen }]">
       <div class="shopping__cart__inner">
         <!-- 標題列 -->
         <div class="shp__cart__title">
@@ -15,7 +15,7 @@
         <!-- 內容區 -->
         <div class="shp__cart__content">
           <!-- 空購物車 -->
-          <div v-if="!cartData?.carts?.length" class="cartempty text-center">
+          <div v-if="!cartStore.cartData?.carts?.length" class="cartempty text-center">
             <h5 class="mt-2">購物清單為空</h5>
             <ul class="shopping__btn">
               <li class="shp__checkout">
@@ -27,7 +27,7 @@
           <!-- 有商品 -->
           <div v-else>
             <div class="shp__cart__wrap">
-              <div class="shp__single__product" v-for="item in cartData.carts" :key="item.id">
+              <div class="shp__single__product" v-for="item in cartStore.cartData.carts" :key="item.id">
                 <div class="shp__pro__thumb">
                   <RouterLink to="/products">
                     <img :src="item.product.imageUrl" alt="product images" />
@@ -47,17 +47,9 @@
             </div>
 
             <ul class="shoping__total">
-              <li class="subtotal">小計: <span class="total__price">NT ${{ cartData.final_total }}</span></li>
-              <li class="subtotal">運費: <span class="total__price">NT $0</span></li>
-              <li class="lump">總金額: <span class="total__price">NT ${{ cartData.final_total }}</span></li>
-            </ul>
-
-            <ul class="shopping__btn">
-              <li>
-                <RouterLink to="/cart" @click="handleClose">查看購物車</RouterLink>
-              </li>
+              <li class="subtotal">小計: <span class="total__price">NT ${{ cartStore.cartData.final_total }}</span></li>
               <li class="shp__checkout">
-                <RouterLink to="/checkout" @click="handleClose">送出訂單</RouterLink>
+                <RouterLink to="/checkout">結帳</RouterLink>
               </li>
             </ul>
           </div>
@@ -68,41 +60,29 @@
 </template>
 
 <script setup>
-import { defineProps, defineEmits } from 'vue'
-import axios from 'axios'
-import { useMessageStore } from '../stores/messageStore'
-
-const props = defineProps({
-  isCartOpen: Boolean,
-  cartData: Object
-})
-
-const emit = defineEmits(['close', 'refresh-cart'])
-
-const messageStore = useMessageStore()
+import { useCartStore } from '../stores/cartStore';
+const cartStore = useCartStore();
 
 const handleClose = () => {
-  emit('close')
-}
+  cartStore.isCartOpen = false;
+};
 
-// 刪除購物車商品
 const removeCartItem = async (id) => {
   try {
-    const apiUrl = import.meta.env.VITE_API_URL
-    const apiPath = import.meta.env.VITE_API_PATH
-    const res = await axios.delete(`${apiUrl}/v2/api/${apiPath}/cart/${id}`)
-    messageStore.setMessage({
-      title: '成功刪除商品',
-      text: res.data.message,
-      type: 'success'
-    })
-    emit('refresh-cart')
-  } catch (err) {
-    messageStore.setMessage({
-      title: '刪除失敗',
-      text: err?.response?.data?.message || '刪除時發生錯誤',
-      type: 'danger'
-    })
+    const res = await cartStore.removeCartItem(id);
+    await cartStore.getCart();
+  } catch (error) {
+    console.error('刪除商品失敗', error);
   }
-}
+};
 </script>
+
+<style scoped>
+.shopping__cart {
+  transition: all 0.3s ease;
+}
+
+.shopping__cart__on {
+  right: 0;
+}
+</style>
